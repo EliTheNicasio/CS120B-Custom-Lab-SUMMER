53e523758650049164b92774790bb41fe8126081
Elijah Nicasio <ElijahN8@gmail.com>
2018-08-22 13:49:05 -0700

    Inital push, project creation

---
+++ Custom Project/Custom Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Custom Project.c
+ *
+ * Created: 8/22/2018 1:42:21 PM
+ * Author : Admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
